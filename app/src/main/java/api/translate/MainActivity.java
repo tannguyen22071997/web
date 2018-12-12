61a24809020b869fc579b767685c3b2cc30ffb0d
TanNguyen <15130167@st.hcmuaf.edu.vn>
2018-12-13 00:04:23 +0700

    update

--- app/src/main/java/api/translate/MainActivity.java
+++ app/src/main/java/api/translate/MainActivity.java
@@ -2,12 +2,36 @@ package api.translate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtnoidung;
+    Button buttonClick;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constrant);
+        setContentView(R.layout.quytrinh);
+        buttonClick = findViewById(R.id.buttonClick);
+        editText =findViewById(R.id.editTextnoidung);
+        txtnoidung = (TextView) findViewById(R.id.noidung);
+        final Random random=new Random();
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a=random.nextInt(20-10+1)+10;
+                txtnoidung.setText(a+"");
+                String nd=editText.getText().toString();
+                Toast.makeText(MainActivity.this,nd,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
